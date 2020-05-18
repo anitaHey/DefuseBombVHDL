6e0387a396ad679eb747bba271ee2f1a102193f7
Marco Wang <m.aesophor@gmail.com>
2020-05-18 21:53:18 +0800

    Allows displaying numbers input via keypad
    The pins `DE1` ~ `DE3` specifies "which row" (only one row at a time)
    of the 4x3 keypad can receive input from user.
    
    000 - 1st row
    100 - 2nd row
    010 - 3rd row
    110 - 4th row
    
    The remaining pins `RK1` ~ `RK3` specifies "which column" (only one
    column at a time) of the 4x3 keypad HAS been pressed.
    
    011 - 1st col
    101 - 2nd col
    110 - 3rd col
    
    The position of "0" indicates which button is pressed.
    
    ---
    
    Problem we face now:
    
    Currently, the keypad's input can be enabled for one row at a time.
    Apparently we will need to apply sequential logic to our circuits
    in order to "enable all rows for receiving input" by updating
    DE1 ~ DE3 at very fast speed.

--- test/key_to_seg7/keyboard.vhd
+++ test/key_to_seg7/keyboard.vhd
@@ -2,27 +2,37 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity keyboard is
-	port(	num_in:in std_logic_vector(5 downto 0);
-			num_out:out std_logic_vector(3 downto 0));
+	port(
+		--clk     : in  std_logic;
+		num_in:in std_logic_vector(5 downto 0);
+		num_out : out std_logic_vector(3 downto 0));  -- value (in binary)
 end keyboard;
 
 -- 0-9 number
 -- 10,11 use for A and B
 -- 12 * , 13 #
 architecture a of keyboard is
-	begin
-		with num_in select
-			num_out <= "0001" when "000100",
-				  "0010" when "000010",
-				  "0011" when "000001",
-				  "0100" when "001100",
-				  "0101" when "001010",
-				  "0110" when "001001",
-				  "0111" when "010100",
-				  "1000" when "010010",
-				  "1001" when "010001",
-				  "0000" when "011010",
-				  "1100" when "011100",
-				  "1101" when "011001",
-				  "1110" when others;
-	end a;
+--signal op : std_logic;
+begin
+	--process(clk)
+	--begin
+	--	if (rising_edge(clk)) then
+	--	end if;
+	--	
+	--end process;
+	
+	with num_in select
+		num_out <=  "0001" when "000011",
+						"0010" when "000101",
+						"0011" when "000110",
+						"0100" when "100011",
+						"0101" when "100101",
+						"0110" when "100110",
+						"0111" when "010011",
+						"1000" when "010101",
+						"1001" when "010110",
+						"0000" when "001011",
+						"1100" when "001101",
+						"1101" when "001110",
+						"1110" when others;
+end a;
